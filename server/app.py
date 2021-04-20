63fde67710f04dd40903c77b0f045a31f5feddf2
Ajay Chopra <Ajay Chopra>
2021-04-20 15:10:15 -0500

    Flask backend running on localhost

--- server/app.py
+++ server/app.py
@@ -1,7 +1,58 @@
-from flask import Flask
+from flask import current_app, g, Flask, flash, jsonify, redirect, render_template, request, session, Response
+import logging
+import json
+import requests
+import datetime
+from db import DB, KeyNotFound, BadRequest
 
 app = Flask(__name__)
+db = DB()
 
+# Hello world route to test connection
 @app.route("/")
 def index():
     return "Hello World!"
+
+
+# user registration
+@app.route("/api/register", methods=["POST"])
+def register_user():
+    """
+    Adds a new user to the database
+    """
+    post_body = request.json
+    if not post_body: 
+        return Response(response="No post body", status=400)
+    try:
+        db.add_user(post_body)
+    except BadRequest as e:
+        return Response(response=e.message, status=400)
+
+    return Response(status=201)
+
+
+# user login
+@app.route("/api/login", methods=["GET"])
+def login_user():
+    """
+    Checks is a user's provided credentials are correct
+    """
+    body = request.json
+    if not body:
+        return Response(response="No body", status=400)
+    try:
+        return jsonify({"validated" : db.verify_user_credentials(body)}) 
+    except BadRequest as e:
+        return Response(response=e.message, status=400)
+    except KeyNotFound as e:
+        return Response(response=e.message, status=400)
+    return Response(status=400)
+
+
+
+    
+
+
+
+
+
---
+++ server/db.py
@@ -0,0 +1,76 @@
+# helper function that converts query result to json, after cursor has executed query
+def to_json(cursor):
+    results = cursor.fetchall()
+    headers = [d[0] for d in cursor.description]
+    return [dict(zip(headers, row)) for row in results]
+
+
+# Error class for when a key is not found
+class KeyNotFound(Exception):
+    def __init__(self, message=None):
+        Exception.__init__(self)
+        if message:
+            self.message = message
+        else:
+            self.message = "Key/Id not found"
+
+    def to_dict(self):
+        rv = dict()
+        rv['message'] = self.message
+        return rv
+
+
+# Error class for when request data is bad
+class BadRequest(Exception):
+    def __init__(self, message=None, error_code=400):
+        Exception.__init__(self)
+        if message:
+            self.message = message
+        else:
+            self.message = "Bad Request"
+        self.error_code = error_code
+
+    def to_dict(self):
+        rv = dict()
+        rv['message'] = self.message
+        return rv
+
+
+# Stand-in local database that we will use until we integrate MySQL
+class DB:
+    def __init__(self):
+        self.users = {} # Holds all user data, indexed by user's email
+    
+    def add_user(self, post_body):
+        '''
+        Add a new email and password to the database
+        '''
+        try:
+            user_email = post_body["email"]
+            user_password = post_body["password"]
+        except KeyError as e:
+            raise BadRequest(message=" Required attribute is missing.")
+        
+        if user_email in self.users:
+            already_present_msg = " User with email: {} is already present".format(user_email)
+            raise BadRequest(message=already_present_msg)
+
+        self.users[user_email] = user_password
+
+    
+    def verify_user_credentials(self, post_body):
+        '''
+        Check if user credentials given in post_body matches records in self.users
+        '''
+        try:
+            user_email = post_body["email"]
+            user_password = post_body["password"]
+        except KeyError as e:
+            raise BadRequest(message=" Required attribute is missing.")
+
+        if not user_email in self.users:
+            raise KeyNotFound(message=" User with email: {} is not present".format(user_email))
+
+        return self.users[user_email] == user_password        
+
+
